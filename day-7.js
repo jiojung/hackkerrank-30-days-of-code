066ccba81441821f9fec66a75cd324ef6cb96238
jio <jiojung@hotmail.com>
2020-06-22 10:03:19 -0400

    finished day-7

--- day-7.js
+++ day-7.js
@@ -1,31 +1,10 @@
-'use strict';
-
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
-});
-
-process.stdin.on('end', _ => {
-    inputString = inputString.replace(/\s*$/, '')
-        .split('\n')
-        .map(str => str.replace(/\s*$/, ''));
-
-    main();
-});
-
-function readLine() {
-    return inputString[currentLine++];
-}
-
-
-
 function main() {
     const n = parseInt(readLine(), 10);
-
+    
     const arr = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    let newarr = [];
+    for(let i=1; i<n+1; i++){
+        newarr.push(arr[arr.length-i])
+    }
+    console.log(newarr.join(" "));
 }
